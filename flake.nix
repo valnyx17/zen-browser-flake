3499325b196c38e068302f6cadc33674b7de760e
vivian <hi@dessa.dev>
2025-03-22 19:58:39 -0400

    update to 1.10.1b

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.6b";
+      version = "1.10.1b";
       downloadUrl = {
         "specific" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
